d9ba139da4104390fccfd6c8929b5990b1fd0842
manikiranp <manikiranp3@gmail.com>
2020-10-11 15:07:31 +0530

    flipping coin n times

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,9 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 echo "Welcome to Coinflip simulator"
-x=$(($RANDOM % 2))
+read -p "Enter number of times you want to simulate the coin flip: " n
+echo "Simulating for $n times:"
+for (( i=1; i<=$n; i++ ))
+do
+x=$((RANDOM%2))
 if [ $x -eq 1 ]; then
 	echo "Heads is the Winner!"
+	((count1++))
 else
 	echo "Tails is the Winner!"
+	((count2++))
 fi
+done
+echo "Heads count: $count1"
+echo "Tails count: $count2"
